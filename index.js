bbaf63c0f915aac0a493878ec67b2945b91b04e9
Checkmate990 <108345290+Checkmate990@users.noreply.github.com>
2023-01-12 22:48:19 -0700

    Update index.js
    wow

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(argument1 = 'roller-skate') {
+    return `This Saturday, I want to ${argument1}!`
+  }
+
+function mondayWork(work ='go to the office'){
+    return `This Monday, I will ${work}.`
+}
+
+
+function wrapAdjective(adj){
+    return function(special){
+        return `You are ${adj}${special}${adj}!` 
+    }
+}
